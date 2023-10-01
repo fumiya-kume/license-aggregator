3a620ff394d78993226b3663740c6f61fbc25896
fumiya-kume <fumiya.kume@hotmail.com>
2023-10-01 15:55:45 +0900

    Add maven config for compose-ui lib

--- license-aggregator-compose/build.gradle.kts
+++ license-aggregator-compose/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    `maven-publish`
 }
 
 android {
@@ -35,6 +36,7 @@ android {
     composeOptions {
         kotlinCompilerExtensionVersion = "1.4.5"
     }
+
 }
 
 dependencies {
@@ -45,4 +47,63 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
 
     implementation("androidx.compose.material3:material3")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            groupId = "systems.kuu"
+            artifactId = "license-aggregator.compose"
+            version = "0.0.1"
+//            repositories {
+//                maven {
+//                    val releasesRepoUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+//                    val snapshotsRepoUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+//                    url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+//                    credentials {
+//                        username =
+//                            System.getenv("sonatype_username") ?: project.findProperty("sonatype_username") as? String
+//                                    ?: ""
+//                        password =
+//                            System.getenv("sonatype_password") ?: project.findProperty("sonatype_password") as? String
+//                                    ?: ""
+//                    }
+//                }
+//            }
+//            from(components["java"])
+//            versionMapping {
+//                usage("java-api") {
+//                    fromResolutionOf("runtimeClasspath")
+//                }
+//                usage("java-runtime") {
+//                    fromResolutionResult()
+//                }
+//            }
+//            pom {
+//                name.set("JpNHolidayK")
+//                description.set("A concise description of my library")
+//                packaging = "jar"
+//                url.set(ProjectProperties.Url.site)
+//                version = ProjectProperties.versionName
+//                licenses {
+//                    license {
+//                        name.set("The Apache License, Version 2.0")
+//                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+//                    }
+//                }
+//                developers {
+//                    developer {
+//                        id.set("kuu")
+//                        name.set("Kuu")
+//                        email.set("fumiya.kume@hotmail.com")
+//                    }
+//                }
+//                scm {
+//                    connection.set("scm:git:git@github.com:fumiya-kume/JpNHolidayK.git")
+//                    developerConnection.set("scm:git:ssh://github.com:fumiya-kume/JpNHolidayK.git")
+//                    url.set(ProjectProperties.Url.site)
+//                }
+//            }
+        }
+    }
 }
