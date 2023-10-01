213f48173cfb639e3f1ef8c8b06801c470fc137b
fumiya-kume <fumiya.kume@hotmail.com>
2023-10-01 15:44:27 +0900

    Remove dead code

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,17 +5,7 @@ pluginManagement {
         gradlePluginPortal()
         mavenLocal()
     }
-//    plugins{
-//        kotlin("jvm") version "1.9.0"
-//        id("com.google.devtools.ksp") version "1.9.0-1.0.11"
-//        id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
-//        id("org.jetbrains.kotlin.android") version "1.8.10" apply false
-////        id("com.android.library")
-////        id("systems.kuu.license-aggregator") version "1.0.0"
-//    }
 }
-
-
 
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
@@ -25,7 +15,6 @@ dependencyResolutionManagement {
         mavenLocal()
     }
 }
-
 
 //rootProject.name = "license-aggregator"
 include("workload")
--- workload/build.gradle.kts
+++ workload/build.gradle.kts
@@ -1,8 +1,6 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
-//    id("systems.kuu.license-aggregator")
-//    id("systems.kuu.license-aggregator") version "1.0.0"
 }
 
 android {
@@ -34,24 +32,10 @@ android {
     }
 }
 
-//buildscript {
-//    repositories {
-//        google()
-//        mavenCentral()
-//        mavenLocal()
-//    }
-//    dependencies {
-//        classpath("systems.kuu:license-aggregator:1.0.0")
-//    }
-//}
-
 apply(plugin = "systems.kuu.license-aggregator.analyzer")
 apply(plugin = "systems.kuu.license-aggregator.generator")
 
 dependencies {
-//    implementation("systems.kuu.license-aggregator:1.0.0")
-//    ksp(project(":license-aggregator"))
-
     implementation(project(":license-aggregator-compose"))
 
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
