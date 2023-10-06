84c0ac6c96f72b2fd691d29d6d5bc2ff3035aca3
fumiya-kume <fumiya.kume@hotmail.com>
2023-10-06 15:08:45 +0900

    Comment out unnecessary dependency

--- license-aggregator/build.gradle.kts
+++ license-aggregator/build.gradle.kts
@@ -11,7 +11,7 @@ dependencies {
     implementation(gradleApi())
     implementation(localGroovy())
     implementation("com.android.tools.build:gradle:8.1.2")
-    implementation("com.android.tools.build:gradle-api:8.1.2")
+//    implementation("com.android.tools.build:gradle-api:8.1.2")
     testImplementation(kotlin("test"))
 }
 
