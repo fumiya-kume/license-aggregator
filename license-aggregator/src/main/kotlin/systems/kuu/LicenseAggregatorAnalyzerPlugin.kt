2fe60ed5c87106df045bde156373612460fefeb6
fumiya-kume <fumiya.kume@hotmail.com>
2023-10-06 19:00:42 +0900

    Remove unused imports in LicenseAggregatorAnalyzerPlugin
    Removed unused imports from LicenseAggregatorAnalyzerPlugin.kt to cleanse the code and improve readability. The unused imports were 'com.android.build.gradle.LibraryExtension' and 'org.gradle.kotlin.dsl.configure'.

--- license-aggregator/src/main/kotlin/systems/kuu/LicenseAggregatorAnalyzerPlugin.kt
+++ license-aggregator/src/main/kotlin/systems/kuu/LicenseAggregatorAnalyzerPlugin.kt
@@ -1,10 +1,8 @@
 package systems.kuu
 
-import com.android.build.gradle.LibraryExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.tasks.diagnostics.DependencyReportTask
-import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.register
 import org.slf4j.LoggerFactory
 
