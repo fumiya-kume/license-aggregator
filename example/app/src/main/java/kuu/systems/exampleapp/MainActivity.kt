5c671e65b0797502af31be31e9a2f2136db34038
fumiya-kume <fumiya.kume@hotmail.com>
2023-10-08 11:02:59 +0900

    Revert "Update Example app"
    This reverts commit 2e89b2e4c01011591b56c818144eb17307e24ac6.

--- example/app/src/main/java/kuu/systems/exampleapp/MainActivity.kt
+++ example/app/src/main/java/kuu/systems/exampleapp/MainActivity.kt
@@ -11,7 +11,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import kuu.systems.exampleapp.ui.theme.ExampleAppTheme
-import systems.kuu.LicenseAggregatorGenerateTask
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,8 +20,6 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
                     Greeting("Android")
-
-
                 }
             }
         }
--- example/settings.gradle.kts
+++ example/settings.gradle.kts
@@ -1,21 +1,15 @@
-import java.net.URI
-
 pluginManagement {
     repositories {
         google()
         mavenCentral()
         gradlePluginPortal()
-        maven { setUrl("https://jitpack.io") }
     }
 }
-
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
-//        maven { url = URI("https://jitpack.io") }
-        maven { setUrl("https://jitpack.io") }
     }
 }
 
