0d539fbe4f1698c6b6ee7da5337515b2e53b8d69
fumiya-kume <fumiya.kume@hotmail.com>
2023-10-08 11:02:59 +0900

    Revert "Remove workload module"
    This reverts commit 9fc78852b15e74f3f9eacc0d17514a1a30cf4415.

---
+++ workload/build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "system.kuu.mylibrary"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 27
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+//apply(plugin = "systems.kuu.license-aggregator.analyzer")
+//apply(plugin = "systems.kuu.license-aggregator.generator")
+
+dependencies {
+    implementation(project(":license-aggregator-compose"))
+
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
+}
+
