c695881ce7f687e71fdd499046ed710c5d7654da
fumiya-kume <fumiya.kume@hotmail.com>
2023-10-08 11:02:57 +0900

    Revert "Update Android tools Gradle version in license-aggregator"
    This reverts commit 86ddf25dd159cf5d015a969cec8d768170106940.

--- license-aggregator/build.gradle.kts
+++ license-aggregator/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 dependencies {
     implementation(gradleApi())
     implementation(localGroovy())
-    implementation("com.android.tools.build:gradle:8.1.2")
+    implementation("com.android.tools.build:gradle:8.0.0")
     testImplementation(kotlin("test"))
 }
 
